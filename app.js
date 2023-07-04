33d7780da732ef2f280548b67d500a72eabb2a86
ViktorBobrov <bobrov.voktor.93@gmail.com>
2023-07-04 22:29:26 +0700

    fix:rezin

--- app.js
+++ app.js
@@ -2,12 +2,8 @@ let acc = document.getElementsByClassName("accordion");
 let i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Переключение между добавлением и удалением класса "active",
-    чтобы выделить кнопку, управляющую панелью */
+  acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
-
-    /* Переключение между скрытием и отображением активной панели */
     let panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
